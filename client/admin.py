a20a1aeb14009038dcf259c0381456e83b05e9a0
Jose Jimenez <luis.jimenez@code-ing.com>
2021-08-05 03:03:55 -0500

    [FIX] requeriments

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from client.models import Pais
 
 # Register your models here.
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ('id_pais', 'codigo_postal', 'nombre', 'ciudad')
+    #search_fields = ('ch_name_business', 'ch_tradename', 'ch_abbreviation')
+    #resource_class = BusinessResource
+    #inlines = [TemplateLookAndFeelInLine, ConfigSendMailInLine]
+
+
+admin.site.register(Pais, PaisAdmin)
