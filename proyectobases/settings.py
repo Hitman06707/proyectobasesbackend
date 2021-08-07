d9744b068154d1d3b09616284c72520289e82ca2
Jose Jimenez <luis.jimenez@code-ing.com>
2021-08-07 01:55:47 -0500

    [FIX] Heroku libs

--- proyectobases/settings.py
+++ proyectobases/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,24 +75,24 @@ WSGI_APPLICATION = 'proyectobases.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'zjlpeeor',
-       'USER': 'zjlpeeor',
-       'PASSWORD': 'PjYjgvh8jyZpUrdSyrJRGITQhFPlWT5V',
-       'HOST': 'chunee.db.elephantsql.com',
-       'PORT': '5432',
-       'CONN_MAX_AGE': 0
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'zjlpeeor',
+#        'USER': 'zjlpeeor',
+#        'PASSWORD': 'PjYjgvh8jyZpUrdSyrJRGITQhFPlWT5V',
+#        'HOST': 'chunee.db.elephantsql.com',
+#        'PORT': '5432',
+#        'CONN_MAX_AGE': 0
+#    }
+# }
 
 
 
@@ -134,5 +133,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
